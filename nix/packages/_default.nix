0ec944203d2916799e3a53213faef903d6e0aa5d
Anton Plotnikov <plotnikovanton@gmail.com>
2025-07-10 12:21:50 +0300

    Nix: add darwin support

--- nix/packages/_default.nix
+++ nix/packages/_default.nix
@@ -1,19 +1,20 @@
 {
   lib,
   buildGoModule,
+  stdenv,
 }:
 buildGoModule {
   pname = "autobrowser";
   version = "0";
-  vendorHash = "sha256-dvu80fFm3vIBjhk9k9Z5h9J5qTbvl3Tq1MCMQVJ+ru8=";
+  vendorHash = if stdenv.isDarwin then "sha256-w/MoA6uOgbQVPFzApJEDLeEFviKbvKjpdaIltyZ3he0=" else "sha256-dvu80fFm3vIBjhk9k9Z5h9J5qTbvl3Tq1MCMQVJ+ru8=";
   meta = with lib; {
     homepage = "https://github.com/pltanton/autobrowser";
     description = "Automatically determine browser depending on provided rules";
     license = licenses.gpl3Only;
-    platforms = platforms.linux;
+    platforms = platforms.linux ++ platforms.darwin;
     mainProgram = "autobrowser";
   };
   src = import ../src.nix {inherit lib;};
 
-  modRoot = "linux";
+  modRoot = if stdenv.isDarwin then "macos" else "linux";
 }
